34011ba478027833aa43ad3c211b041210006895
codingbycarter <102538933+codingbycarter@users.noreply.github.com>
2022-04-28 18:13:31 -0400

    Use UpperCase Method to Solve

---
+++ MakeUpperCase.js
@@ -0,0 +1,8 @@
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+  
+// let answer = str.toUpperCase();
+// return answer
+}
+ // https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
