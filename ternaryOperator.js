27d41ca359997fc836c3b70b1016bcfa93de5fde
Codingbycarter <you@example.com>
2022-05-16 02:38:56 -0400

    ifElseternary

---
+++ ternaryOperator.js
@@ -0,0 +1,18 @@
+#  if..else and ternary operator
+saleHotdogs =( n )->
+#   //show me the code!
+ if n < 5
+    100*n
+  else if 5 <= n < 10
+    95*n
+  else
+    90*#  if..else and ternary operator
+
+
+
+
+
+ 
+
+
+
