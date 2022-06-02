38a6090902bd288919e9a65a1844fab75c747243
Resilient Coders <resilientcoders@Resilients-MacBook-Pro.local>
2022-06-02 16:47:26 -0400

    swappFNforLN

---
+++ nameShuffler.js
@@ -0,0 +1,11 @@
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+//Shuffle It
+//Similar to Palindrome code
+
+function nameShuffler(name) {
+  return name.split(" ").reverse().join(" ")
+}
+
