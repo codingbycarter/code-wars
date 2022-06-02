240c2f4ed78775fca19d5d6c3e6fdb5425b3c987
Resilient Coders <resilientcoders@Resilients-MacBook-Pro.local>
2022-06-02 15:30:01 -0400

    solutions

---
+++ evenOrodd.js
@@ -0,0 +1,8 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function even_or_odd(number) {
+  if (number = 0 || Number.isInteger(number/2)) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
